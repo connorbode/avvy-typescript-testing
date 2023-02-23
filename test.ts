e3e75fe018c91df40d37aa338ef1a58b6b6ed6fa
Connor Bode <connor.bode@gmail.com>
2023-02-22 20:55:11 -0500

    init

---
+++ test.ts
@@ -0,0 +1,15 @@
+import AVVY from '@avvy/client'
+import { ethers } from 'ethers'
+
+const main = async () => {
+  // const provider = new ethers.providers.JsonRpcProvider('https://api.avax.network/ext/bc/C/rpc')
+  const provider = new ethers.JsonRpcProvider('https://api.avax.network/ext/bc/C/rpc')
+  const avvy = AVVY(provider, {})
+  const names = await avvy.batch([
+    '0x9BC4e7C1Fa4Ca66f6B2F4B6F446Dad80Ec541983',
+  ]).reverseToNames(3)
+  console.log(names)
+}
+
+main()
+
